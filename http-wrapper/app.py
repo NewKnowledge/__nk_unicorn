ae1c9bb272fa3b95e586fe5479336ceec41ac2dc
Garrett Honke <garretthonke@gmail.com>
2018-07-05 12:16:37 -0400

    app updates

--- http-wrapper/app.py
+++ http-wrapper/app.py
@@ -7,15 +7,15 @@ from json import dumps, loads
 import pandas as pd
 from keras.applications.inception_v3 import InceptionV3
 from nk_unicorn import *
-from utils import requires_auth
+from utils import *
 
 
 class UnicornRestListener():
-    """ **Class for managing UNICORN config**
+    ''' **Class for managing UNICORN config**
         UnicornRestListener accepts an image path or URL
         and outputs a json file with the character and object
         information that has been deteected.
-    """
+    '''
 
     def __init__(self):
         self.unicorn = Unicorn()
@@ -54,35 +54,39 @@ app = Flask(__name__)
 @app.route('/')
 # @requires_auth
 def index():
-    """ **Demo Landing Route**
-    """
+    ''' **Demo Landing Route**
+    '''
     return render_template('index.html')
 
+# to have a nice web demo we need code here to take a list of
+# paths POSTed as a string
 
-@app.route("/demo-fileupload", methods=['POST'])
-# @requires_auth
-def demo_cluster_uploaded_images():
-    ''' **Demo Results Route**
-        Listen for an image url being POSTed on root.
-    '''
-    request.get_data()
+# @app.route("/demo-fileupload", methods=['POST'])
+# # @requires_auth
+# def demo_cluster_uploaded_images():
+#     ''' **Demo Results Route**
+#         Listen for an image url being POSTed on root.
+#     '''
+#     request.get_data()
 
-    image_paths = request.form['image_paths']
+#     image_paths = request.form['image_paths']
 
-    result = listener.find_clusters(image_paths)
+#     result = listener.find_clusters(image_paths)
 
-    # Converting to pd.DataFrame seemed to be the fastest way to get results to a http table
-    return render_template(
-        'display.html', image_paths=image_paths,
-        table=pd.DataFrame.from_dict(result).to_html(),
-    )
+#     # Converting to pd.DataFrame seemed to be the fastest way to get results to a http table
+#     return render_template(
+#         'display.html', image_paths=image_paths,
+#         table=pd.DataFrame.from_dict(result).to_html(),
+#     )
 
 
 @app.route("/test-fileupload", methods=['POST'])
 # @requires_auth
 def test_cluster_uploaded_images():
-    ''' **Route for using UNICOR as a http/web service**
-        Listen for an image url being POSTed on root.
+    ''' **Route for testing UNICORN as a http/web service**
+        Listen for a JSONified dict being POSTed on root where
+        the only entry is a key-value pair where the key is
+        'image_paths' and the value is a list of of image urls
     '''
     request.get_data()
 
@@ -99,10 +103,26 @@ def cluster_uploaded_images():
     ''' **Route for using UNICOR as a http/web service**
         Listen for an image url being POSTed on root.
     '''
-    request.get_data()
 
-    image_paths = loads(request.get_json())['image_paths']
+    test_query = '''
+        select
+            l.url
+        from social.links l
+        join social.posts_links pl on pl.link_id=l.link_id
+        join social.posts p on pl.post_id=p.post_id
+        join social.communities_posts cp on cp.post_id=p.post_id
+            and cp.community_slug='col_b'
+            where l.type='image';
+    '''
 
-    result = listener.find_clusters(image_paths)
+    db_connection = connect(seed=True)
 
-    return app.response_class(result, content_type='application/json')
+    query_result = execute_query(db_connection, test_query)
+
+    print(query_result)
+
+    # image_paths = loads(request.get_json())['image_paths']
+
+    # result = listener.find_clusters(image_paths)
+
+    # return app.response_class(result, content_type='application/json')
--- http-wrapper/utils.py
+++ http-wrapper/utils.py
@@ -1,40 +1,70 @@
-from os import getenv
+import os
 from functools import wraps
 
+import psycopg2
+from retrying import retry
 from flask import Response, request
+from config import SQL_HOST, SQL_PASSWORD, SQL_PORT, SQL_USER
 
-authorized_user = {
-    "user": getenv("API_USER", "admin"),
-    "pass": getenv("API_PASSWORD")
-}
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+POSTGRES_HOST_SEED = os.getenv('POSTGRES_HOST_SEED', 'localhost')
 
-if authorized_user["user"] is "admin":
-    print("[WARN]: Using default username")
+# authorized_user = {
+#     "user": os.getenv("API_USER", "admin"),
+#     "pass": os.getenv("API_PASSWORD")
+# }
 
-if authorized_user["pass"] is None:
-    print("[WARN]: authorized_user password is not set")
+# if authorized_user["user"] is "admin":
+#     print("[WARN]: Using default username")
 
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
+# if authorized_user["pass"] is None:
+#     print("[WARN]: authorized_user password is not set")
 
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == authorized_user["user"] and password == authorized_user["pass"]
+# def requires_auth(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         auth = request.authorization
+#         if not auth or not check_auth(auth.username, auth.password):
+#             return authenticate()
+#         return f(*args, **kwargs)
+#     return decorated
 
 
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
-        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+# def check_auth(username, password):
+#     """This function is called to check if a username /
+#     password combination is valid.
+#     """
+#     return username == authorized_user["user"] and password == authorized_user["pass"]
+
+
+# def authenticate():
+#     """Sends a 401 response that enables basic auth"""
+#     return Response(
+#         'Could not verify your access level for that URL.\n'
+#         'You have to login with proper credentials', 401,
+#         {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def execute_query(db_connection, query, query_params={}):
+    print('executing query')
+    with db_connection as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query, query_params)
+            if cursor.description is not None:
+                return list(cursor.fetchall())
+            else:   
+                return True
+
+
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000,
+       stop_max_attempt_number=5)
+def connect(seed=False):
+    host = POSTGRES_HOST_SEED if seed else POSTGRES_HOST
+    return psycopg2.connect(
+        host=host,
+        dbname=POSTGRES_USER,
+        user=POSTGRES_USER,
+        port=5432,
+        sslmode='allow')
