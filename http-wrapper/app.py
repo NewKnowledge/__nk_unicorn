b6d9dbfad3c752bcd1e6af608bd38ba78adc31a7
Craig Corcoran <craig@newknowledge.io>
2018-07-08 15:42:10 -0500

    fix num image limit query param, set docker-compose to use remote dbs.

--- http-wrapper/app.py
+++ http-wrapper/app.py
@@ -40,12 +40,13 @@ def req_visual_clusters(community_name):
     # localhost:5000/visual-clusters/world-cup?start_time=2018-07-06_18:00:00
 
     stop_time = request.args.get('stop_time', to_date_string(pd.datetime.now()))
-    start_time = request.args.get('start_time', to_date_string(pd.datetime.now() - pd.Timedelta('2d')))
-    image_limit = request.args.get('image_limit', None)
+    start_time = request.args.get('start_time', to_date_string(pd.datetime.now() - pd.Timedelta('3d')))
+    image_limit = request.args.get('image_limit', 600)
 
     print('getting list of urls for community named', community_name,
           'from', start_time, 'to', stop_time, file=sys.stderr)
 
+    # TODO pass limit to community images query
     image_urls = get_community_image_urls(community_name, start_time, stop_time)
 
     if not image_urls:
@@ -53,8 +54,8 @@ def req_visual_clusters(community_name):
 
     if image_limit:
         # pick a random `image_limit` images to use for clustering
-        inds = np.random.permutation(len(image_urls))[:image_limit]
-        image_urls = image_urls[inds]
+        np.random.shuffle(image_urls)
+        image_urls = image_urls[:image_limit]
 
     # print('community image urls:', image_urls, file=sys.stderr)
     print('len of results of community image url query:', len(image_urls), file=sys.stderr)
@@ -77,132 +78,9 @@ def req_visual_clusters(community_name):
     insert_clusters(community_name, image_urls, labels)
     print('insert successful', file=sys.stderr)
 
-    # print('urls by label:', urls_by_label, file=sys.stderr)
     return jsonify(json.loads(json.dumps(urls_by_label, cls=PandasEncoder)))
+    # json.dumps(urls_by_label, cls=PandasEncoder)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# class UnicornRestListener():
-#     ''' **Class for managing UNICORN config**
-#         UnicornRestListener accepts an image path or URL
-#         and outputs a json file with the character and object
-#         information that has been deteected.
-#     '''
-
-#     def __init__(self):
-#         self.unicorn = Unicorn()
-#         self.unicorn.model = InceptionV3(weights='imagenet', include_top=False)
-#         self.cluster_cutoff = 2
-
-#     def cluster_stats(self, cluster_data):
-#         ''' produce some stats about the cluster outcome
-#         '''
-#         key_clusters_df = pd.concat(
-#             g for _, g in cluster_data.groupby('cluster_label')
-#             if len(g) >= self.cluster_cutoff
-#         )
-
-#         cluster_stats_df = pd.DataFrame()
-
-#         for i in key_clusters_df['cluster_label'].unique():
-#             temp_data = key_clusters_df[key_clusters_df['cluster_label'] == i]
-
-#             cluster_stats_df = cluster_stats_df.append(
-#                 {'cluster_size': len(temp_data),
-#                  'mean_variance': np.mean(temp_data.iloc[:, 2:].var(axis=0))
-#                  }, ignore_index=True
-#             )
-
-#         return cluster_stats_df
-
-#     def find_clusters(self, image_paths):
-#         ''' analyze a given image and return text and detected objects
-#         '''
-
-#         unicorn_result = self.unicorn.cluster_images(image_paths)
-
-#         return unicorn_result
-
-
-# @app.route('/visual-clusters/<int:community_name>')
-# def req_image_clusters(community_name: int):
-
-#     stop_time = request.args.get('stop_time', to_date_string(pd.datetime.now()))
-#     start_time = request.args.get('start_time', to_date_string(pd.datetime.now() - pd.Timedelta('2d')))
-#     cluster_limit = int(request.args.get('cluster_limit', 10))
-#     url_limit = int(request.args.get('url_limit', 100))
-#     pretty = request.args.get('pretty', False)
-
-#     res = get_image_clusters(community_name, start_time, stop_time,
-#                              cluster_limit=cluster_limit,
-#                              url_limit=url_limit,
-#                              )
-
-#     if pretty:
-#         res = json.loads(json.dumps(res, cls=PandasEncoder))
-#         return jsonify(res)
-#     return json.dumps(res, cls=PandasEncoder, indent=2)
-
-
-# @app.route('/visual-clusters/<int:community_name>')
-# def test_cluster_uploaded_images():
-#     ''' **Route for testing UNICORN as a http/web service**
-#         Listen for a JSONified dict being POSTed on root where
-#         the only entry is a key-value pair where the key is
-#         'image_paths' and the value is a list of of image urls
-#     '''
-#     request.get_data()
-
-#     image_paths = loads(request.get_json())['image_paths']
-
-#     result, processed_feature_data = listener.find_clusters(image_paths)
-
-#     cluster_stats_df = listener.cluster_stats(
-#         result.join(processed_feature_data)
-#     )
-
-#     return app.response_class(
-#         cluster_stats_df.to_json(),
-#         content_type='application/json')
-
-
-# # Init UNICORN Class def'd above
-# listener = UnicornRestListener()
-# # Hack-y work around for ensuring Keras Tensor map is available in the global scope.
-# # See https://github.com/keras-team/keras/issues/2397 for more info
-# listener.find_clusters(
-#     ["http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg",
-#      "http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg",
-#      "http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg",
-#      "http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg",
-#      "http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg",
-#      "http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg"]
-# )
-# @app.route('/')
-# @requires_auth
-# def index():
-#     ''' **Demo Landing Route**
-#     '''
-    # return render_template('index.html')
-
-# to have a nice web demo we need code here to take a list of
-# paths POSTed as a string and clean it up
-# @app.route("/demo-fileupload", methods=['POST'])
-# # @requires_auth
-# def demo_cluster_uploaded_images():
-#     ''' **Demo Results Route**
-#         Listen for an image url being POSTed on root.
-#     '''
-#     request.get_data()
-
-#     image_paths = request.form['image_paths']
-
-#     result = listener.find_clusters(image_paths)
-
-#     # Converting to pd.DataFrame seemed to be the fastest way to get results to a http table
-#     return render_template(
-#         'display.html', image_paths=image_paths,
-#         table=pd.DataFrame.from_dict(result).to_html(),
-#     )
