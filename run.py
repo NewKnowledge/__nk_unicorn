2399eb75bb0d7479cba817e0b9f8387c4741dc67
Poff Poffenberger <poffdeluxe@gmail.com>
2018-07-17 14:45:51 -0500

    Updating logging and adding dynamic communities

--- run.py
+++ run.py
@@ -1,29 +1,33 @@
 import os
+import logging
 
 import pandas as pd
 
-from http_wrapper import get_visual_clusters, insert_clusters, remove_community_clusters
+from http_wrapper import get_community_names, get_visual_clusters, insert_clusters, remove_community_clusters
 from http_wrapper.http_utils import to_date_string
 
-# TODO query social db for all communities to cluster
-COMMUNITIES = os.getenv('COMMUNITIES', ['world-cup', 'test-community'])
+logging.basicConfig(level=logging.DEBUG)
+
 START_TIME = os.getenv('START_TIME', to_date_string(pd.datetime.now() - pd.Timedelta('14d')))
 STOP_TIME = os.getenv('STOP_TIME', to_date_string(pd.datetime.now()))
 IMAGE_LIMIT = os.getenv('IMAGE_LIMIT', 1000)
 
-for community_name in COMMUNITIES:
+communities = get_community_names()
+logging.info(f'COMMUNITIES: {communities}')
+
+for community_name in communities:
     try:
-        print('clustering community:', community_name)
-        result = get_visual_clusters(community_name, START_TIME, STOP_TIME, image_limit=IMAGE_LIMIT)
-        if isinstance(result, str):
-            print(result)
-        else:
-            image_urls, labels = result
+        logging.info(f'clustering community: {community_name}')
+        image_urls, labels = get_visual_clusters(community_name, START_TIME, STOP_TIME, image_limit=IMAGE_LIMIT)
+
+        if not image_urls:
+            # Skip to next community if we didn't get any images
+            continue
 
         removed = remove_community_clusters(community_name)
-        print('removed', len(removed), 'rows')
+        logging.info(f'removed {len(removed)} rows')
 
         insert_clusters(community_name, image_urls, labels)
-        print('inserted', len(image_urls), 'rows')
+        logging.info(f'inserted {len(image_urls)} rows')
     except Exception as err:
-        print('community', community_name, 'failed with error:', err)
+        logging.exception(f'community {community_name} failed')
